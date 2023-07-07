4768b035ff23bc563c63ac4b4b22d919fd92ac07
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-07-07 14:15:02 +0530

    some change

--- src/Wipro/togglStr.java
+++ src/Wipro/togglStr.java
@@ -2,4 +2,7 @@ package Wipro;
 
 public class togglStr {
 
+    public void togglee(String str, int num) {
+    }
+
 }
--- src/Wipro/toggleStr.java
+++ src/Wipro/toggleStr.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class toggleStr {
+
+    String togglee(String input1, int input2) {
+        return "";
+    }
+
     public static void main(String[] args) {
         String str;
         int num;
-        Scanner sc = new Scanner
-        toggleStr obj = new toggleStr();
-
-
+        Scanner sc = new Scanner(System.in);
+        str = sc.next();
+        num = sc.nextInt();
+        togglStr obj = new togglStr();
+        obj.togglee(str, num);
     }
 
 }
