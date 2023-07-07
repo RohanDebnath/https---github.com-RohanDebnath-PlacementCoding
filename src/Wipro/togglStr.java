b0b687671c4c7a15195cbb21a4c9974e75e92a8b
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-07-08 04:52:12 +0530

    Toggle STR

--- src/Wipro/togglStr.java
+++ src/Wipro/togglStr.java
@@ -1,8 +1,63 @@
-package Wipro;
+
+import java.util.Scanner;
 
 public class togglStr {
 
-    public void togglee(String str, int num) {
+    String result(String input1, int input2) {
+
+        StringBuilder sb = new StringBuilder();
+        if (input2 == 0) {
+            for (int i = 0; i < input1.length(); i += 2) {
+                char ch = input1.charAt(i);
+                if (Character.isUpperCase(ch)) {
+                    char ch1 = Character.toLowerCase(input1.charAt(i));
+                    sb.append(ch1);
+                }
+                if (Character.isLowerCase(ch)) {
+                    char ch1 = Character.toUpperCase(input1.charAt(i));
+                    sb.append(ch1);
+                }
+            }
+        } else if (input2 == 1) {
+            for (int i = 1; i < input1.length(); i += 2) {
+                char ch = input1.charAt(i);
+                if (Character.isUpperCase(ch)) {
+                    char ch1 = Character.toLowerCase(input1.charAt(i));
+                    sb.append(ch1);
+                }
+                if (Character.isLowerCase(ch)) {
+                    char ch1 = Character.toUpperCase(input1.charAt(i));
+                    sb.append(ch1);
+                }
+            }
+        } else if (input2 == 2) {
+            StringBuilder sb1 = new StringBuilder(input1);
+            input1 = sb1.reverse().toString();
+            for (int i = 0; i < input1.length(); i++) {
+                char ch = input1.charAt(i);
+                if (Character.isUpperCase(ch)) {
+                    char ch1 = Character.toLowerCase(input1.charAt(i));
+                    sb.append(ch1);
+                }
+                if (Character.isLowerCase(ch)) {
+                    char ch1 = Character.toUpperCase(input1.charAt(i));
+                    sb.append(ch1);
+                }
+            }
+        }
+
+        return sb.toString();
+
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input1;
+        int input2;
+        input1 = sc.nextLine();
+        input2 = sc.nextInt();
+
+        togglStr obj = new togglStr();
+        System.out.println(obj.result(input1, input2));
+    }
 }
