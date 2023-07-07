050b25a253ac878ebfb302d001f44c94d497a7f9
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-07-07 13:04:26 +0530

    test

---
+++ src/Wipro/toggleStr.java
@@ -0,0 +1,11 @@
+public class toggleStr {
+    public static void main(String[] args) {
+        String str;
+        int num;
+        Scanner sc = new Scanner
+        toggleStr obj = new toggleStr();
+
+
+    }
+
+}
