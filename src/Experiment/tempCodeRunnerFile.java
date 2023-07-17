70e10f9a315d4154fbb09308d014950f813d1ce6
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-07-17 09:57:43 +0530

    Morning Stress

--- src/Experiment/tempCodeRunnerFile.java
+++ src/Experiment/tempCodeRunnerFile.java
@@ -1,6 +1,30 @@
-class tempCodeRunnerFile {
+public class tempCodeRunnerFile {
+    public static boolean isAutobiographical(int number) {
+        String numString = String.valueOf(number);
+        int[] frequency = new int[numString.length()];
+
+        // Count the frequency of each digit in the number
+        for (int i = 0; i < numString.length(); i++) {
+            int digit = Character.getNumericValue(numString.charAt(i));
+            if (digit < frequency.length) {
+                frequency[digit]++;
+            }
+        }
+
+        // Check if the frequency matches the digits
+        for (int i = 0; i < frequency.length; i++) {
+            int digit = Character.getNumericValue(numString.charAt(i));
+            if (digit != frequency[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-      char ch='a';
-      System.out.println(ch-'0');
+        long  number = 6210001000;
+        boolean isAutobiographical = isAutobiographical(number);
+        System.out.println(number + " is autobiographical? " + isAutobiographical);
     }
 }
