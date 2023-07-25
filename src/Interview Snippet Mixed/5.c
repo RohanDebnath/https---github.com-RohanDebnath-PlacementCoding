00fd02ce620af049e9a000b027b7364639ca0546
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-07-25 23:02:03 +0530

    #define NULL

---
+++ src/Interview Snippet Mixed/5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i = 4, j = 7;
+    // while (++i < --j)
+    // {
+    //     printf("loop\n");
+    // }
+
+    while (i++ < j--)
+    {
+        printf("loop\n");
+    }
+}
---
+++ src/Interview Snippet Mixed/6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define NULL 0
+int main()
+{
+    while (NULL==0)
+    {
+        printf("While Loop");
+        break;
+    }
+    
+}
