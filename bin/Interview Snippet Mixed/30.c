eb3adacb4a3ed2df4603713a38c942598e73f411
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-07-26 20:50:36 +0530

    Java Static var

---
+++ bin/Interview Snippet Mixed/30.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int j;
+    for(int i=0, j=5;i<j;i++,j--)
+    {
+        printf("%d %d ",i,j);
+    }
+}
---
+++ src/Interview Snippet Mixed/java3.java
@@ -0,0 +1,11 @@
+
+public class java3 {
+int i=5;
+static public void show()
+{
+    System.out.println(i);
+}
+public static void main(String[] args) {
+    java3.show();
+}    
+}
