3ed85922fc170b24eb9807dd8ef58415765caf86
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-07-27 20:58:35 +0530

    commit

---
+++ src/Experiment/bamby.java
@@ -0,0 +1,19 @@
+
+ class rohan {
+      void result()
+    {
+        System.out.println("Bambytika");
+    }
+}
+    class bamby extends rohan
+    {
+         void result()
+        {
+            System.out.println("tatata tataaaa taaaaaaaaa tutut tre tuuu tuuu");
+        }
+    public static void main(String[] args) {
+        rohan obj= new bamby();
+        obj.result();
+       // result();
+    }    
+}
