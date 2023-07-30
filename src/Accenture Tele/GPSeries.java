beecacfc82f8c99acf44292665309492fb07c707
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-07-31 02:36:18 +0530

    GP

--- src/Accenture Tele/GPSeries.java
+++ src/Accenture Tele/GPSeries.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 
 public class GPSeries {
-public static void main(String[] args) {
-    
-}    
+    static double result(int st, int tr, int n) {
+        int r =  tr / st;
+        int a = st;
+        return a * Math.pow(r, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int st = sc.nextInt();
+        int tt = sc.nextInt();
+        System.out.println(result(st, tt, n));
+    }
 }
