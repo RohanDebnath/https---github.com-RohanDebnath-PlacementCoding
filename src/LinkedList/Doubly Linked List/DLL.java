d67bce5992a81330da28b06bbf6ec9e54c876a63
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-08-17 06:36:03 +0530

    deleteFirst node of DLL

---
+++ src/LinkedList/Doubly Linked List/DLL.java
@@ -0,0 +1,118 @@
+import java.util.NoSuchElementException;
+
+public class DLL {
+    private ListNode head;
+    private ListNode tail;
+    private int len;
+
+    private class ListNode {
+        private int data;
+        private ListNode next;
+        private ListNode prev;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public DLL() {
+        this.head = null;
+        this.tail = null;
+        this.len = 0;
+    }
+
+    public boolean isEmpty() {
+        return len == 0; // head==null
+    }
+
+    public void insertAtLast(int val) {
+        ListNode newNode = new ListNode(val);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        newNode.prev = tail;
+        tail = newNode;
+        len++;
+    }
+
+    public void displayForward() {
+        if (head == null)
+            return;
+        else {
+            ListNode temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+            System.out.print("Null");
+        }
+    }
+
+    public void displayBackward() {
+        if (tail == null)
+            return;
+        else {
+            ListNode temp = tail;
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.prev;
+            }
+            System.out.print("Null");
+        }
+    }
+
+    public void insertAtfirst(int val) {
+        ListNode newNode = new ListNode(val);
+        if (isEmpty()) {
+            tail = newNode;
+        } else {
+            head.prev = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public ListNode deleteatFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        ListNode temp = head;
+        if (head == tail) {
+            tail = null;
+        } else {
+            head.next.prev = null;
+        }
+        head = head.next;
+        temp.next = null;
+        len--;
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        DLL dll = new DLL();
+        dll.insertAtLast(10);
+        dll.insertAtLast(20);
+        dll.insertAtLast(30);
+
+        dll.displayForward();
+        System.out.println();
+        dll.displayBackward();
+
+        dll.insertAtfirst(5);
+
+        System.out.println();
+        dll.displayForward();
+        System.out.println();
+        dll.displayBackward();
+
+        System.out.println();
+        dll.deleteatFirst();
+        dll.displayForward();
+        System.out.println();
+        dll.displayBackward();
+
+    }
+}
--- src/Silly FIlly Test/Soumik.java
+++ src/Silly FIlly Test/Soumik.java
@@ -1,32 +1,17 @@
-public class Soumik {
-    public static String getTown(String[] peopleNames) {
-    
-        String firstString = peopleNames[0].toLowerCase(); 
+ class Soumik {
 
-        int minStringLength = firstString.length();
-
-        for (String str : peopleNames) {
-            minStringLength = Math.min(minStringLength, str.length());
-        }
-
-        StringBuilder commonPrefix = new StringBuilder();
-
-        for (int i = 0; i < minStringLength; i++) {
-            char currentChar = firstString.charAt(i);
-            for (int j = 1; j < peopleNames.length; j++) {
-                if (Character.toLowerCase(peopleNames[j].charAt(i)) != currentChar) { 
-                    return commonPrefix.toString();
-                }
-            }
-            commonPrefix.append(currentChar);
-        }
-
-        return commonPrefix.toString();
-    }
-
+   static int a=6;
+  void result()
+  { 
+     a++;
+     System.out.println(a);
+  }
     public static void main(String[] args) {
-        String[] inputStrings = {"Rosewood", "rose", "rosy"};
- 
-        System.out.println("Common Prefix: " + getTown(inputStrings));
+     
+        Soumik obj = new Soumik();
+        obj.result();
+        a++;
+        System.out.println(Soumik.a);
+     
     }
 }
