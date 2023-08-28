aef0b1616e63dc22ed53e228801fce1881b46eca
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-08-28 14:13:32 +0530

    Trainne Oxygen

---
+++ src/TCS/TraineeOxygen.java
@@ -0,0 +1,83 @@
+
+/*
+ Selection of MPCS exams include a fitness test which is conducted on ground. There will be a batch of 3 trainees, appearing for running test in track for 3 rounds. You need to record their oxygen level after every round. After trainee are finished with all rounds, calculate for each trainee his average oxygen level over the 3 rounds and select one with highest oxygen level as the most fit trainee. If more than one trainee attains the same highest average level, they all need to be selected.
+
+Display the most fit trainee (or trainees) and the highest average oxygen level.
+
+Note:
+
+The oxygen value entered should not be accepted if it is not in the range between 1 and 100.
+If the calculated maximum average oxygen value of trainees is below 70 then declare the trainees as unfit with meaningful message as “All trainees are unfit.
+Average Oxygen Values should be rounded.
+Example 1:
+INPUT VALUES
+95
+92
+95
+92
+90
+92
+90
+92
+90
+
+OUTPUT VALUES
+Trainee Number : 1
+Trainee Number : 3
+
+Note:
+Input should be 9 integer values representing oxygen levels entered in order as
+
+Round 1
+
+Oxygen value of trainee 1
+Oxygen value of trainee 2
+Oxygen value of trainee 3
+Round 2
+
+Oxygen value of trainee 1
+Oxygen value of trainee 2
+Oxygen value of trainee 3
+Round 3
+
+Oxygen value of trainee 1
+Oxygen value of trainee 2
+Oxygen value of trainee 3
+
+
+*/
+import java.util.Scanner;
+
+public class TraineeOxygen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t1 = 0, t2 = 0, t3 = 0;
+        int arr[] = new int[9];
+        for (int i = 0; i < 9; i++) {
+            try {
+                arr[i] = sc.nextInt();
+            } catch (Exception e) {
+
+                System.out.println("Invalid Input");
+                return;
+            }
+
+        }
+        for (int i = 0; i < 9; i += 3) {
+            t1 += arr[i];
+            t2 += arr[i + 1];
+            t3 += arr[i + 2];
+        }
+        t1 /= 3;
+        t2 /= 3;
+        t3 /= 3;
+
+        int max = Math.max(t1, Math.max(t2, t3));
+        if (t1 == max)
+            System.out.println("Trainee Number:" + 1);
+        if (t2 == max)
+            System.out.println("Trainee Number:" + 2);
+        if (t3 == max)
+            System.out.println("Trainee Number:" + 3);
+    }
+}
